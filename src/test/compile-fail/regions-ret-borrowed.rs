ddfaa959034e6a13684d712930ee69fbc89b45f3
Niko Matsakis <niko@alum.mit.edu>
2014-08-12 10:06:34 -0400

    test -- same general error, different cause, manifests slightly differently

--- src/test/compile-fail/regions-ret-borrowed.rs
+++ src/test/compile-fail/regions-ret-borrowed.rs
@@ -20,9 +20,11 @@ fn with<R>(f: |x: &int| -> R) -> R {
 }
 
 fn return_it<'a>() -> &'a int {
-    with(|o| o) //~ ERROR mismatched types
-        //~^ ERROR lifetime of return value does not outlive the function call
+    with(|o| o)
+        //~^ ERROR mismatched types
         //~^^ ERROR cannot infer
+        //~^^^ ERROR not valid during the expression
+        //~^^^ ERROR not valid at this point
 }
 
 fn main() {
