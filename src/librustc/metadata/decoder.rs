3270a38ced59ab779df42b35bb959631c87d2068
Eduard Burtescu <edy.burt@gmail.com>
2015-01-30 11:26:49 +0200

    rustc: remove unused ForeignType item family.

--- src/librustc/metadata/decoder.rs
+++ src/librustc/metadata/decoder.rs
@@ -119,7 +119,6 @@ enum Family {
     StaticMethod,          // F
     Method,                // h
     Type,                  // y
-    ForeignType,           // T
     Mod,                   // m
     ForeignMod,            // n
     Enum,                  // t
@@ -144,7 +143,6 @@ fn item_family(item: rbml::Doc) -> Family {
       'F' => StaticMethod,
       'h' => Method,
       'y' => Type,
-      'T' => ForeignType,
       'm' => Mod,
       'n' => ForeignMod,
       't' => Enum,
@@ -344,7 +342,7 @@ fn item_to_def_like(item: rbml::Doc, did: ast::DefId, cnum: ast::CrateNum)
                 _ => panic!()
             }
         }
-        Type | ForeignType => DlDef(def::DefTy(did, false)),
+        Type => DlDef(def::DefTy(did, false)),
         Mod => DlDef(def::DefMod(did)),
         ForeignMod => DlDef(def::DefForeignMod(did)),
         StructVariant => {
