32ca2dcf8a04cb53ce8fc548380e6b56b8d94543
toidiu <toidiu@protonmail.com>
2017-11-18 20:11:36 -0500

    fix license

--- src/librustc_typeck/outlives/explicit.rs
+++ src/librustc_typeck/outlives/explicit.rs
@@ -1,13 +1,13 @@
-//// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
-//// file at the top-level directory of this distribution and at
-//// http://rust-lang.org/COPYRIGHT.
-////
-//// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-//// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-//// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-//// option. This file may not be copied, modified, or distributed
-//// except according to those terms.
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
 //
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //#![allow(unused)]
 //#[allow(dead_code)]
 //
@@ -25,27 +25,29 @@
 //    assert_eq!(crate_num, LOCAL_CRATE);
 //
 //    //let mut explicit_outlives_predicates = map();
-//    let explicit_predicates = FxHashMap();
+//    let mut explicit_predicates = FxHashMap();
 //
-//    let mut visitor = ExplicitVisitor {
-//        tcx,
-//        explicit_predicates,
-//        crate_num,
-//    };
-//    //iterate over all the crates
-//    tcx.hir.krate().visit_all_item_likes(&mut visitor);
+//    {
+//        let mut visitor = ExplicitVisitor {
+//            tcx,
+//            &mut explicit_predicates,
+//            crate_num,
+//        };
+//        //iterate over all the crates
+//        tcx.hir.krate().visit_all_item_likes(&mut visitor);
+//    }
 //
 //    explicit_predicates
 //}
 //
 //
-//pub struct ExplicitVisitor<'tcx> {
+//pub struct ExplicitVisitor<'tcx, 'a> {
 //    tcx: TyCtxt<'tcx, 'tcx, 'tcx>,
-//    explicit_predicates: FxHashMap<DefId, Vec<String>>,
+//    explicit_predicates: &'a FxHashMap<DefId, Vec<String>>,
 //    crate_num: CrateNum,
 //}
 //
-//impl<'tcx, 'v> ItemLikeVisitor<'v> for ExplicitVisitor<'tcx> {
+//impl<'tcx, 'a, 'v> ItemLikeVisitor<'v> for ExplicitVisitor<'tcx, 'a> {
 //    fn visit_item(&mut self, item: &hir::Item) {
 //
 //        // Compute a map from each struct/enum/union S to the **explicit**
@@ -56,8 +58,8 @@
 //        // for the type.
 //        //for def_id in all_types() {
 //        //    let explicit_predicates = tcx.explicit_predicates(def_id);
-//        let def_id = LocalDefId::new(item.hir_id.owner).to_def_id();
-//        //let def_id = DefId{ krate: self.crate_num, index: item.hir_id.owner, }
+//        //let def_id = LocalDefId::new(item.hir_id.owner).to_def_id();
+//        let def_id = DefId{ krate: self.crate_num, index: item.hir_id.owner, }
 //        let local_explicit_predicate = self.tcx.explicit_predicates_of(def_id);
 //
 //        //    let filtered_predicates = explicit_predicates.iter()
