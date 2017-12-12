3bbfcb0342c2054e7ff4a52e5cdc4f379437cca9
toidiu <toidiu@protonmail.com>
2017-12-11 23:49:54 -0500

    iterate over the items and match on enum, struct, and union

--- src/librustc_typeck/outlives/implicit_infer.rs
+++ src/librustc_typeck/outlives/implicit_infer.rs
@@ -12,6 +12,7 @@
 #[allow(dead_code)]
 
 use rustc::ty::{self, CratePredicatesMap, TyCtxt};
+use rustc::ty::{AdtKind, ToPolyTraitRef, Ty};
 use rustc::ty::maps::Providers;
 use std::rc::Rc;
 use util::nodemap::FxHashMap;
@@ -20,6 +21,10 @@ use rustc::hir;
 use rustc::hir::itemlikevisit::ItemLikeVisitor;
 use rustc::hir::def_id::{self, CrateNum, DefId, LOCAL_CRATE};
 use std::collections::HashSet;
+use rustc::ty::{ToPredicate, ReprOptions};
+use syntax_pos::{Span, DUMMY_SP};
+use rustc::hir::def::{Def, CtorKind};
+use syntax::{abi, ast};
 
 // Create the sets of inferred predicates for each type.
 
@@ -72,28 +77,100 @@ pub struct ImplicitVisitor<'a, 'p: 'a, 'cx, 'tcx: 'cx> {
 
 impl<'a, 'p, 'cx, 'v, 'tcx> ItemLikeVisitor<'v> for ImplicitVisitor<'a, 'p, 'cx, 'tcx> {
     fn visit_item(&mut self, item: &hir::Item) {
+
+        use rustc::hir::map::*;
+        use rustc::hir::*;
+
         *self.changed = false;
 
         let def_id = DefId {
             krate: *self.crate_num,
             index: item.hir_id.owner,
         };
-        let adt_def = self.tcx.adt_def(def_id);
-        let mut required_predicates: HashSet<i32> = HashSet::new();
 
-        //        for variant in adt_def OB{
-        //            for field in variant {
-        //                // from ty/outlives.rs
-        //                //   Foo<'b, 'c>  ==> ['b, 'c]
-        //                //   Vec<T>: 'a
-        //                //   outlives_components(Vec<T>) = [T]
-        //                let outlives = tcx.outlives_components(field_ty);
-        //                required_predicates.extend(required_predicates_for_type_to_be_wf(field_ty));
-        //            }
-        //        }
-        //        inferred_outlives_predicates.extend(required_predicates);
-        //        if new predicates were added { changed = true; }
-        //    }
+        let mut required_predicates: HashSet<i32> = HashSet::new();
+        // let adt_def = local_adt_def(self.tcx, def_id);
+
+        let node_id = self.tcx.hir.as_local_node_id(def_id).unwrap();
+        let item = match self.tcx.hir.get(node_id) {
+            NodeItem(item) => item,
+            _ => bug!()
+        };
+
+        match item.node {
+            ItemUnion(ref def, _) => {
+            //     (AdtKind::Union, vec![
+            //         convert_struct_variant(tcx, def_id, item.name,
+            //                                ty::VariantDiscr::Relative(0), def)
+            //     ])
+            }
+            ItemEnum(ref def, _) => {
+            //     let mut distance_from_explicit = 0;
+            //     (AdtKind::Enum, def.variants.iter().map(|v| {
+            //         let did = tcx.hir.local_def_id(v.node.data.id());
+            //         let discr = if let Some(e) = v.node.disr_expr {
+            //             distance_from_explicit = 0;
+            //             ty::VariantDiscr::Explicit(tcx.hir.local_def_id(e.node_id))
+            //         } else {
+            //             ty::VariantDiscr::Relative(distance_from_explicit)
+            //         };
+            //         distance_from_explicit += 1;
+
+            //         convert_struct_variant(tcx, did, v.node.name, discr, &v.node.data)
+            //     }).collect())
+            }
+
+            ItemStruct(ref def, _) => {
+                // // Use separate constructor id for unit/tuple structs and reuse did for braced structs.
+                // let ctor_id = if !def.is_struct() {
+                //     Some(tcx.hir.local_def_id(def.id()))
+                // } else {
+                //     None
+                // };
+                //let fields = def.fields().iter().map(|f| {
+                //    //let fid = tcx.hir.local_def_id(f.id);
+                //    ////let dup_span = seen_fields.get(&f.name).cloned();
+                //    ////if let Some(prev_span) = dup_span {
+                //    ////    struct_span_err!(tcx.sess, f.span, E0124,
+                //    ////                     "field `{}` is already declared",
+                //    ////                     f.name)
+                //    ////        .span_label(f.span, "field already declared")
+                //    ////        .span_label(prev_span, format!("`{}` first declared here", f.name))
+                //    ////        .emit();
+                //    ////} else {
+                //    ////    seen_fields.insert(f.name, f.span);
+                //    ////}
+
+                //    ty::FieldDef {
+                //        // did: fid,
+                //        did: tcx.hir.local_def_id(f.id),
+                //        name: f.name,
+                //        vis: ty::Visibility::from_hir(&f.vis, node_id, tcx)
+                //    }
+                //}).collect();
+
+
+                // ----------- example
+                for field in def.fields().iter() {
+                    //        for variant in adt_def {
+                    //            for field in variant {
+                    //                // from ty/outlives.rs
+                    //                //   Foo<'b, 'c>  ==> ['b, 'c]
+                    //                //   Vec<T>: 'a
+                    //                //   outlives_components(Vec<T>) = [T]
+                    // let outlives = tcx.outlives_components(field_ty);
+                    // required_predicates.extend(required_predicates_for_type_to_be_wf(field_ty));
+                    //            }
+                    //        }
+                    //        inferred_outlives_predicates.extend(required_predicates);
+                    //        if new predicates were added { changed = true; }
+
+                }
+
+            }
+            _ => {} //bug!()
+        };
+
 
     }
 
@@ -101,3 +178,105 @@ impl<'a, 'p, 'cx, 'v, 'tcx> ItemLikeVisitor<'v> for ImplicitVisitor<'a, 'p, 'cx,
 
     fn visit_impl_item(&mut self, impl_item: &hir::ImplItem) {}
 }
+
+//fn local_adt_def<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>,
+//                     def_id: DefId)
+//                     -> &'tcx ty::AdtDef {
+//    use rustc::hir::map::*;
+//    use rustc::hir::*;
+
+//    let node_id = tcx.hir.as_local_node_id(def_id).unwrap();
+//    let item = match tcx.hir.get(node_id) {
+//        NodeItem(item) => item,
+//        _ => bug!()
+//    };
+
+//    // let repr = ReprOptions::new(tcx, def_id);
+//    let (kind, variants) = match item.node {
+//        ItemEnum(ref def, _) => {
+//            let mut distance_from_explicit = 0;
+//            (AdtKind::Enum, def.variants.iter().map(|v| {
+//                let did = tcx.hir.local_def_id(v.node.data.id());
+//                let discr = if let Some(e) = v.node.disr_expr {
+//                    distance_from_explicit = 0;
+//                    ty::VariantDiscr::Explicit(tcx.hir.local_def_id(e.node_id))
+//                } else {
+//                    ty::VariantDiscr::Relative(distance_from_explicit)
+//                };
+//                distance_from_explicit += 1;
+//
+//                convert_struct_variant(tcx, did, v.node.name, discr, &v.node.data)
+//            }).collect())
+//        }
+//        ItemStruct(ref def, _) => {
+//            // Use separate constructor id for unit/tuple structs and reuse did for braced structs.
+//            let ctor_id = if !def.is_struct() {
+//                Some(tcx.hir.local_def_id(def.id()))
+//            } else {
+//                None
+//            };
+//            (AdtKind::Struct, vec![
+//                convert_struct_variant(tcx, ctor_id.unwrap_or(def_id), item.name,
+//                                       ty::VariantDiscr::Relative(0), def)
+//            ])
+//        }
+//        ItemUnion(ref def, _) => {
+//            (AdtKind::Union, vec![
+//                convert_struct_variant(tcx, def_id, item.name,
+//                                       ty::VariantDiscr::Relative(0), def)
+//            ])
+//        }
+//        _ => bug!()
+//    };
+
+//    // let def = ty::AdtDef::new(self.tcx, did, kind, variants, repr);
+//    // self.tcx.global_arenas.adt_def.alloc(def)
+//}
+
+//// pub fn alloc_adt_def(self,
+////                      did: DefId,
+////                      kind: AdtKind,
+////                      variants: Vec<ty::VariantDef>,
+////                      repr: ReprOptions)
+////                      -> &'gcx ty::AdtDef {
+////     let def = ty::AdtDef::new(self, did, kind, variants, repr);
+////     self.global_arenas.adt_def.alloc(def)
+//// }
+
+//fn convert_struct_variant<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>,
+//                                    did: DefId,
+//                                    name: ast::Name,
+//                                    discr: ty::VariantDiscr,
+//                                    def: &hir::VariantData)
+//                                    -> ty::VariantDef {
+//    // let mut seen_fields: FxHashMap<ast::Name, Span> = FxHashMap();
+//    let node_id = tcx.hir.as_local_node_id(did).unwrap();
+//    let fields = def.fields().iter().map(|f| {
+//        //let fid = tcx.hir.local_def_id(f.id);
+//        ////let dup_span = seen_fields.get(&f.name).cloned();
+//        ////if let Some(prev_span) = dup_span {
+//        ////    struct_span_err!(tcx.sess, f.span, E0124,
+//        ////                     "field `{}` is already declared",
+//        ////                     f.name)
+//        ////        .span_label(f.span, "field already declared")
+//        ////        .span_label(prev_span, format!("`{}` first declared here", f.name))
+//        ////        .emit();
+//        ////} else {
+//        ////    seen_fields.insert(f.name, f.span);
+//        ////}
+
+//        ty::FieldDef {
+//            // did: fid,
+//            did: tcx.hir.local_def_id(f.id),
+//            name: f.name,
+//            vis: ty::Visibility::from_hir(&f.vis, node_id, tcx)
+//        }
+//    }).collect();
+//    ty::VariantDef {
+//        did,
+//        name,
+//        discr,
+//        fields,
+//        ctor_kind: CtorKind::from_hir(def),
+//    }
+//}
