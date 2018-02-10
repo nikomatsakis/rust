d39a8cc362b820257437ca94c822d5f406dd1859
toidiu <toidiu@protonmail.com>
2018-02-09 20:12:04 -0500

    add sudo code for TyAdt and return a Set rather than a Map when calcualting predicates

--- src/librustc_typeck/outlives/implicit_infer.rs
+++ src/librustc_typeck/outlives/implicit_infer.rs
@@ -83,8 +83,15 @@ impl<'cx, 'tcx> ItemLikeVisitor<'tcx> for InferVisitor<'cx, 'tcx> {
             hir::ItemStruct(..) => {
                 let adt_def = self.tcx.adt_def(def_id);
                 for field_def in adt_def.all_fields() {
-                    let required_predicates = required_predicates_to_be_wf(self.tcx, field_def);
-                    local_required_predicates.extend(required_predicates);
+                    let required_predicates: Vec<_> = required_predicates_to_be_wf(
+                        self.tcx,
+                        field_def,
+                        &local_required_predicates,
+                     ).into_iter().collect();
+                    local_required_predicates.insert(
+                        field_def.did,
+                        Rc::new(required_predicates),
+                    );
                 }
             }
             _ => {}
@@ -92,7 +99,8 @@ impl<'cx, 'tcx> ItemLikeVisitor<'tcx> for InferVisitor<'cx, 'tcx> {
 
         if local_required_predicates.len() > 0 {
             self.changed = true;
-            self.inferred_outlives_map.extend(local_required_predicates);
+            self.inferred_outlives_map
+                .extend(local_required_predicates);
         }
     }
 
@@ -105,32 +113,68 @@ impl<'cx, 'tcx> ItemLikeVisitor<'tcx> for InferVisitor<'cx, 'tcx> {
 fn required_predicates_to_be_wf<'tcx>(
     tcx: TyCtxt<'_, 'tcx, 'tcx>,
     field_def: &ty::FieldDef,
-) -> FxHashMap<DefId, Rc<Vec<ty::Predicate<'tcx>>>> {
+    calculated_predicates: &FxHashMap<DefId, Rc<Vec<ty::Predicate<'tcx>>>>,
+) -> HashSet<ty::Predicate<'tcx>> {
 
-    let mut predicates = FxHashMap();
+    let mut predicates = HashSet::new();
     // Get the type of the field with identity substs applied.  For
     // now, let's just see if that causes horrible cycles.
-    let ty = tcx.type_of(field_def.did);
-    match ty.sty {
+    let field_ty = tcx.type_of(field_def.did);
+    match field_ty.sty {
         // For each type `&'a T`, we require `T: 'a`
+        //
+        // If you have A = &'a T, then mt.ty here represents T and ty represents A (and r represents 'a).
+        // So we want T: 'a.
         ty::TyRef(r, mt) => {
             let outlives_pred = ty::Binder(ty::OutlivesPredicate(mt.ty, r))
                 .to_predicate();
-            predicates.insert(
-                field_def.did,
-                Rc::new(vec![outlives_pred]),
-            );
+            predicates.insert(outlives_pred);
         }
 
         // For each struct/enum/union type `Foo<'a, T>`, we can
         // load the current set of inferred and explicit predicates from
-        // `inferred_outlives_map` and see if those include `T:
-        // 'a`
-        ty::TyAdt(def, substs) => {}
+        // `inferred_outlives_map` and see if those include `T: 'a`
+        ty::TyAdt(def, substs) => {
+
+            // // Get type generics(not lifetimes) of def
+            // // and get lifetime generics of def
+            // let (type_generics, lifetime_generics) = def
+            //     .get_generics()
+            //     .filter(is_type_generic);
+
+            // // Create OutlivesPredicates for each type/lifetime.
+            // // We will use this to test if the same OutlivesPredicate
+            // // also exists in the preciously calculated map
+            // let outlive_pred_pairs = type_generics.map( |t| =>
+            //     lifetime_generics.map( |l| =>
+            //         ty::OutlivesPredicates(t, l)
+            // ));
+
+
+
+            // // For each type generic see if it exists in the
+            // // previously calculated predicates
+            // let ty_exists_in_pred_map = type_generics
+            //     .filter( |g| =>
+            //         calculated_predicates.contains_key(g)
+            //     );
+
+            // // Check if the predicates for type_generic intersects
+            // // with the outlive_pred_pairs. If so then we should return
+            // // the same OutlivesPredicate for this field
+            // ty_exists_in_pred_map.iter( |t| =>
+            //     let add_pred_to_field = calculated_predicates
+            //         .get_predicates(t)
+            //         .intersect(outlive_pred_pairs);
+
+            //     predicates.chain(add_pred_to_field);
+            // );
+
+        }
 
         // For `TyDynamic` types, we can do the same, but using the `expredicates_of`
         // query (those are not inferred).
-        ty::TyDynamic(data, r) => {}
+        ty::TyDynamic(data, r) => { }
 
         _ => {}
 
