6d595a89cbb44521d31c5d57302f5d13aa217f6e
toidiu <toidiu@protonmail.com>
2018-02-11 18:30:31 -0500

    simplify logic for detecting if outlivesPredicate needs to be added

--- src/librustc_typeck/outlives/implicit_infer.rs
+++ src/librustc_typeck/outlives/implicit_infer.rs
@@ -113,7 +113,7 @@ impl<'cx, 'tcx> ItemLikeVisitor<'tcx> for InferVisitor<'cx, 'tcx> {
 fn required_predicates_to_be_wf<'tcx>(
     tcx: TyCtxt<'_, 'tcx, 'tcx>,
     field_def: &ty::FieldDef,
-    calculated_predicates: &FxHashMap<DefId, Rc<Vec<ty::Predicate<'tcx>>>>,
+    infered_outlives_map: &FxHashMap<DefId, Rc<Vec<ty::Predicate<'tcx>>>>,
 ) -> HashSet<ty::Predicate<'tcx>> {
 
     let mut predicates = HashSet::new();
@@ -123,8 +123,8 @@ fn required_predicates_to_be_wf<'tcx>(
     match field_ty.sty {
         // For each type `&'a T`, we require `T: 'a`
         //
-        // If you have A = &'a T, then mt.ty here represents T and ty represents A (and r represents 'a).
-        // So we want T: 'a.
+        // If you have A = &'a T, then mt.ty here represents T and
+        // ty represents A (and r represents 'a). So we want T: 'a.
         ty::TyRef(r, mt) => {
             let outlives_pred = ty::Binder(ty::OutlivesPredicate(mt.ty, r))
                 .to_predicate();
@@ -136,39 +136,76 @@ fn required_predicates_to_be_wf<'tcx>(
         // `inferred_outlives_map` and see if those include `T: 'a`
         ty::TyAdt(def, substs) => {
 
-            // // Get type generics(not lifetimes) of def
-            // // and get lifetime generics of def
-            // let (type_generics, lifetime_generics) = def
-            //     .get_generics()
-            //     .filter(is_type_generic);
+            // Get type generics of def and lifetime(region) generics of def
+            let generics: &ty::Generics = tcx.generics_of(def.did);
+            let gen_types: &Vec<ty::TypeParameterDef> = &generics.types;
+            let gen_region: &Vec<ty::RegionParameterDef> = &generics.regions;
 
-            // // Create OutlivesPredicates for each type/lifetime.
-            // // We will use this to test if the same OutlivesPredicate
-            // // also exists in the preciously calculated map
-            // let outlive_pred_pairs = type_generics.map( |t| =>
-            //     lifetime_generics.map( |l| =>
-            //         ty::OutlivesPredicates(t, l)
-            // ));
+            // Create OutlivesPredicates for each type/lifetime.
+            // We will use this to test if the same OutlivesPredicate
+            // also exists in the infered_outlives_map
+            // let mut outlive_predicate_pairs = Vec::new();
+            // for typ in gen_types {
+            //     for reg in gen_region {
+            //         outlive_predicate_pairs.push(
+            //             ty::OutlivesPredicate(typ, reg)
+            //         );
+            //     };
+            // };
 
 
+            // iterate over all predicates in the infered_outlives_map.
+            // See if there are any OutlivesPredicate kind. If there
+            // exists a type and region that is also in this field
+            // then that should be returned.
+            for (_, p_list) in infered_outlives_map.iter() {
+                for p in p_list.as_ref() {
+                    match p {
+                        // ty::Predicate::Trait(..) |
+                        // ty::Predicate::Equate(..) |
+                        // ty::Predicate::Subtype(..) |
+                        // ty::Predicate::Projection(..) |
+                        // ty::Predicate::ClosureKind(..) |
+                        // ty::Predicate::ObjectSafe(..) |
+                        // ty::Predicate::ConstEvaluatable(..) |
+                        // ty::Predicate::WellFormed(subty) => |
+                        // ty::Predicate::RegionOutlives(ref data) => 5,
 
-            // // For each type generic see if it exists in the
+                        ty::Predicate::TypeOutlives(ref data) => match data {
+                            // ty::OutlivesPredicate(a, b) => {
+                                // if gen_types.contains(typ) && gen_region.contains(reg) {
+                                //     let outlives_pred =
+                                //        ty::Binder(ty::OutlivesPredicate(typ, reg))
+                                //        .to_predicate();
+                                //     predicates.insert(outlives_pred);
+                                // }
+
+                            // },
+
+                            _ => (),
+                        },
+
+                        _ => (),
+                    };
+                }
+            };
+
+            // // For each type generic check if it exists in the
             // // previously calculated predicates
-            // let ty_exists_in_pred_map = type_generics
-            //     .filter( |g| =>
-            //         calculated_predicates.contains_key(g)
-            //     );
+            // let ty_exists_in_calculated_map = gen_types.into_iter()
+            //     .filter( |typ| infered_outlives_map.contains_key(&typ.def_id));
 
             // // Check if the predicates for type_generic intersects
             // // with the outlive_pred_pairs. If so then we should return
             // // the same OutlivesPredicate for this field
-            // ty_exists_in_pred_map.iter( |t| =>
-            //     let add_pred_to_field = calculated_predicates
-            //         .get_predicates(t)
-            //         .intersect(outlive_pred_pairs);
-
-            //     predicates.chain(add_pred_to_field);
-            // );
+            // for ty in ty_exists_in_calculated_map {
+            //     let to_add: &Vec<_> = infered_outlives_map
+            //         .get(&ty.def_id)
+            //         .expect("we know the key exists")
+            //         .as_ref();
+            //     to_add.intersects(outlive_predicate_pairs);
+            //     // predicates.chain(to_add);
+            // }
 
         }
 
