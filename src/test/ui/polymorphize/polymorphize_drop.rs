a0639694b2670bb2e8b4284668c8716db33ef6da
Niko Matsakis <niko@alum.mit.edu>
2018-12-27 06:59:41 -0500

    add a new test showing an undetected kind of dependency (drop)

---
+++ src/test/ui/polymorphize/polymorphize_drop.rs
@@ -0,0 +1,32 @@
+// Simple test for the polymorphize analysis code.
+//
+// compile-flags: -Zpolymorphize -Zpolymorphize-dump
+
+#![allow(warnings)]
+
+struct OffsetDependent<T> {
+    t: T,
+    count: u32,
+}
+
+fn dependency_because_drop_T<T>(data: T) {
+    //~^ ERROR no polymorphic dependencies found
+    //
+    // We have to drop `data`, and to know what to run, we have to know what `T` is.
+    //
+    // FIXME-- we don't presently see the dependency
+}
+
+fn dependency_because_drop_box_T<T>(data: Box<T>) {
+    //~^ ERROR no polymorphic dependencies found
+    //
+    // We have to drop `data`, and to know what to run, we have to
+    // know what `T` is.  This is true even though the *size* of
+    // `data` is known.
+    //
+    // FIXME-- we don't presently see the dependency
+}
+
+fn main() {
+    //~^ ERROR no polymorphic dependencies found
+}
