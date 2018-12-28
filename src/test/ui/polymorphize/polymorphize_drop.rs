3fe620a030e1d96aec5be3f3a32b61566a7a1edd
Niko Matsakis <niko@alum.mit.edu>
2018-12-28 10:55:33 -0500

    some more drop tests

--- src/test/ui/polymorphize/polymorphize_drop.rs
+++ src/test/ui/polymorphize/polymorphize_drop.rs
@@ -18,6 +18,22 @@ fn dependency_because_drop_box_T<T>(data: Box<T>) {
     //~^ ERROR some polymorphic dependencies found
 }
 
+fn no_dependency_because_drop_ref_T<T>(data: &T) {
+    //~^ ERROR no polymorphic dependencies found
+}
+
+fn no_dependency_because_indirect_drop_ref_T<T>(data: &T) {
+    //~^ ERROR no polymorphic dependencies found
+
+    dependency_because_drop_T::<&T>(data);
+}
+
+fn no_dependency_because_indirect_drop_ref_u32() {
+    //~^ ERROR no polymorphic dependencies found
+
+    dependency_because_drop_T::<&u32>(&22);
+}
+
 fn main() {
     //~^ ERROR no polymorphic dependencies found
 }
